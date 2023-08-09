d8b247058f3f3917cbe8874f94d66f93ee5033ea
Azharuet09 <59698441+Azharuet09@users.noreply.github.com>
2023-08-09 03:43:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ client = MongoClient(
 db = client['TravelChatbot']
 hotels_collection = db['hotels']
 
-openai.api_key = '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'
+# openai.api_key = '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'
+openai.api_key = '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
 
 # Memory DF
 df = pd.read_csv("memory.csv")
