259ad42461501ede5a898e229853b4f3e31ea796
Azhar <Azharuet409@gmail.com>
2023-08-11 02:10:39 -0700

    added

--- app.py
+++ app.py
@@ -659,10 +659,10 @@ def hotel_check():
 
 
 if __name__ == '__main__':
-    app.run()
-    # host = 'ec2-3-95-218-42.compute-1.amazonaws.com'
-    # port = 5000
-    # app.run(host=host, port=port)
+    # app.run()
+    host = 'ec2-3-95-218-42.compute-1.amazonaws.com'
+    port = 5000
+    app.run(host=host, port=port)
     # app.run(debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(port=8000, debug=True)
