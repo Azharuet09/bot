553c8a0b900c22d2e59ecb2db9934e4864251123
Azhar <Azharuet409@gmail.com>
2023-08-11 01:10:27 -0700

    added

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ hotels_collection = db['hotels']
 # openai.api_key = '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'
 # openai.api_key = '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'
 # openai.api_key = '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'
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Memory DF
 df = pd.read_csv("memory.csv")
